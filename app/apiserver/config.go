0d64f8c3519050f0dc0fe8387341f346520030c8
Artyom Krasavin <krasavin-00@mail.ru>
2021-04-26 12:36:04 +0300

    logs, first route

--- app/apiserver/config.go
+++ app/apiserver/config.go
@@ -1,11 +1,17 @@
 package apiserver
 
+import "github.com/OldGattsu/go-rest-api/store"
+
 type Config struct {
 	BindAddr string `toml:"bind_addr"`
+	LogLevel string `toml:"log_level"`
+	Store    *store.Config
 }
 
 func NewConfig() *Config {
 	return &Config{
-		BindAddr: ":8080",
+		BindAddr: ":5555",
+		LogLevel: "debug",
+		Store:    store.NewConfig(),
 	}
 }
